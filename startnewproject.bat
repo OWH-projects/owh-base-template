f10053923999e91c76c69b016b48da0bdcd28493
Cody Winchester <cjwinchester@gmail.com>
2014-02-20 23:42:27 -0600

    Delete startnewproject.bat

--- startnewproject.bat
+++
@@ -1,7 +0,0 @@
-:: for Windows users, a batch file to quickly start a new Data Omaha project
-
-cd git
-@echo off
-set /p name="Name your project: " %=%
-git clone https://github.com/OWH-projects/owh-base-template %name%
-%SystemRoot%\explorer.exe "C:\Users\Winchester\Desktop\git\%name%"
